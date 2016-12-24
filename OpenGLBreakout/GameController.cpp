5311596ebe091e67143d227bb33729b4f65c3aa9
mycmessia <164810193@qq.com>
2016-12-24 13:41:16 +0000

    Added player control

--- OpenGLBreakout/GameController.cpp
+++ OpenGLBreakout/GameController.cpp
@@ -12,6 +12,13 @@
 #include "ResourceManager.hpp"
 #include "SpriteRenderer.hpp"
 
+// Initial size of the player paddle
+const glm::vec2 PLAYER_SIZE (100, 20);
+// Initial velocity of the player paddle
+const GLfloat PLAYER_VELOCITY (500.0f);
+
+GameObject* Player;
+
 GameController::GameController (GLuint width, GLuint height)
 : mState (GAME_ACTIVE), mKeys (), mWidth (width), mHeight (height)
 {
@@ -47,6 +54,8 @@ void GameController::Init ()
     ResourceManager::LoadTexture (TEXTURE_FULL_DIR"monsterface.png", GL_TRUE, "face");
     ResourceManager::LoadTexture (TEXTURE_FULL_DIR"block.png", GL_FALSE, "block");
     ResourceManager::LoadTexture (TEXTURE_FULL_DIR"block_solid.png", GL_FALSE, "block_solid");
+    ResourceManager::LoadTexture (TEXTURE_FULL_DIR"paddle.png", true, "paddle");
+    
     // Load levels
     GameLevel one; one.Load (LEVEL_FULL_DIR"one.lvl", this->mWidth, this->mHeight * 0.5);
     GameLevel two; two.Load (LEVEL_FULL_DIR"two.lvl", this->mWidth, this->mHeight * 0.5);
@@ -57,6 +66,10 @@ void GameController::Init ()
     this->Levels.push_back (three);
     this->Levels.push_back (four);
     this->Level = 1;
+    
+    glm::vec2 playerPos = glm::vec2 (this->mWidth / 2 - PLAYER_SIZE.x / 2,
+                                     this->mHeight - PLAYER_SIZE.y);
+    Player = new GameObject (playerPos, PLAYER_SIZE, ResourceManager::GetTexture("paddle"));
 }
 
 void GameController::Update (GLfloat dt)
@@ -67,7 +80,21 @@ void GameController::Update (GLfloat dt)
 
 void GameController::ProcessInput (GLfloat dt)
 {
-    
+    if (this->mState == GAME_ACTIVE)
+    {
+        GLfloat velocity = PLAYER_VELOCITY * dt;
+        // Move playerboard
+        if (this->mKeys[GLFW_KEY_A])
+        {
+            if (Player->Position.x >= 0)
+                Player->Position.x -= velocity;
+        }
+        if (this->mKeys[GLFW_KEY_D])
+        {
+            if (Player->Position.x <= this->mWidth - Player->Size.x)
+                Player->Position.x += velocity;
+        }
+    }
 }
 
 void GameController::Render ()
@@ -79,6 +106,9 @@ void GameController::Render ()
                              glm::vec2(0, 0), glm::vec2(this->mWidth, this->mHeight), 0.0f);
         
         // Draw level
-        this->Levels[this->Level].Draw(*Renderer);
+        this->Levels[this->Level].Draw (*Renderer);
+        
+        Player->Draw (*Renderer);
+
     }
 }
